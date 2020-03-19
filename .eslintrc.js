dcba7a0c0fde573c78781c34b0b066a7a3063b72
Razielini <razielco@gmail.com>
2020-03-18 17:45:57 -0700

    Initial
    Example of a Booking UI for a Hotel
    - Tailwind
    - PostCSS
    - PurgeCSS
    - Typescript

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    '@nuxtjs/eslint-config-typescript'
+  ],
+  // add your custom rules here
+  rules: {
+    "strictPropertyInitialization": 0,
+    "no-v-html": 0,
+    "vue/valid-v-html": 0,
+    "v-html": 0,
+    "prefer-const": 0,
+    "no-console": 0
+  }
+}
---
+++ nuxt.config.js
@@ -0,0 +1,93 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    '@nuxtjs/tailwindcss'
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  tailwindcss: {
+    configPath: '~/plugins/tailwind.full.config.js',
+    cssPath: '~/assets/css/tailwind.css',
+    purgeCSSInDev: true,
+    exposeConfig: false
+  },
+  purgeCSS: {
+    mode: 'postcss'
+  },
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    // Doc: https://github.com/nuxt-community/dotenv-module
+    '@nuxtjs/dotenv',
+    'nuxt-purgecss'
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    extractCSS: true,
+    postcss: {
+      plugins: {
+        tailwindcss: './plugins/tailwind.full.config.js'
+      }
+    }
+    /*
+    ** You can extend webpack config here
+    extend (config, ctx) {
+    }
+    */
+  },
+  typescript: {
+    typeCheck: {
+      eslint: false
+    }
+  },
+  server: {
+    port: 4100, // default: 3000
+  }
+}
